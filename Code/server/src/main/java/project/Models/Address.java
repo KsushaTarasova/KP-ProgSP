1fd7d581697b8bff71bb3c8f32649f2684461c02
KsushaTarasova <ksutaras543@mail.ru>
2024-10-22 23:02:42 +0300

    Add GeneratedValue annotation to id column

--- Code/server/src/main/java/project/Models/Address.java
+++ Code/server/src/main/java/project/Models/Address.java
@@ -1,9 +1,6 @@
 package project.Models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Table(name="address")
 public class Address {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     @Column(name="city")
     String city;
