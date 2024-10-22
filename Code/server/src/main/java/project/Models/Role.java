0c7426784962c6c2c8cb514aa8ff91324624613d
KsushaTarasova <ksutaras543@mail.ru>
2024-10-22 23:44:45 +0300

    Create Role class

---
+++ Code/server/src/main/java/project/Models/Role.java
@@ -0,0 +1,23 @@
+package project.Models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import project.Enums.Roles;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="role_id")
+    int id;
+    @Enumerated(EnumType.STRING)
+    @Column(name="role_name")
+    Roles role;
+}
