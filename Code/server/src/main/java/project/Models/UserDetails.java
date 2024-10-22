e14ded514387eb83a2271b95bf806dd6c2a6beff
KsushaTarasova <ksutaras543@mail.ru>
2024-10-22 23:05:41 +0300

    Create UserDetails class

---
+++ Code/server/src/main/java/project/Models/UserDetails.java
@@ -0,0 +1,32 @@
+package project.Models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_details")
+public class UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column(name = "firstname")
+    String firstname;
+    @Column(name = "lastname")
+    String lastname;
+    @Column(name = "email")
+    String email;
+    @Column(name = "phone")
+    String phone;
+    @Column(name = "icon_url")
+    String iconUrl;
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    Address address;
+}
