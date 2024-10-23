c51b48238f6869320914c2ec4c1f5e89e494a3f4
KsushaTarasova <ksutaras543@mail.ru>
2024-10-23 22:56:24 +0300

    Add fields

--- Code/server/src/main/java/project/Utility/ClientThread.java
+++ Code/server/src/main/java/project/Utility/ClientThread.java
@@ -1,5 +1,14 @@
 package project.Utility;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientThread {
+    private Socket clientSocket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+
 
 }
