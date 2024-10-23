be4153b75fbf2a1720860ec2f86d348b439f3e28
KsushaTarasova <ksutaras543@mail.ru>
2024-10-23 13:00:19 +0300

    Create UserDAO class that implements CRUD interface and override methods for User

---
+++ Code/server/src/main/java/project/DAO/UserDAO.java
@@ -0,0 +1,77 @@
+package project.DAO;
+
+import org.hibernate.Session;
+import project.Interfaces.CRUD;
+import project.Models.User;
+import project.SessionFactory.SessionFactoryImpl;
+
+import java.util.List;
+
+public class UserDAO implements CRUD <User> {
+    @Override
+    public boolean add(User object) {
+        boolean isAdded = false;
+        try(Session session = SessionFactoryImpl.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.persist(object);
+            session.getTransaction().commit();
+            isAdded = true;
+        } catch (NoClassDefFoundError e){
+            System.out.println("Exception: " + e);
+        }
+        return isAdded;
+    }
+
+    @Override
+    public boolean update(User object) {
+        boolean isUpdated = false;
+        try(Session session = SessionFactoryImpl.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.merge(object);
+            session.getTransaction().commit();
+            isUpdated = true;
+        } catch (NoClassDefFoundError e){
+            System.out.println("Exception: " + e);
+        }
+        return isUpdated;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        boolean isDeleted = false;
+        try(Session session = SessionFactoryImpl.getSessionFactory().openSession()){
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.remove(user);
+            session.getTransaction().commit();
+            isDeleted = true;
+        } catch (NoClassDefFoundError e){
+            System.out.println("Exception: " + e);
+        }
+        return isDeleted;
+    }
+
+    @Override
+    public List<User> showAll() {
+        List<User> users = null;
+        try (Session session = SessionFactoryImpl.getSessionFactory().openSession()){
+            users = session.createQuery("From User", User.class).list();
+        } catch (NoClassDefFoundError e){
+            System.out.println("Exception: " + e);
+        }
+        return users;
+    }
+
+    @Override
+    public User findById(int id) {
+        User user = null;
+        try(Session session = SessionFactoryImpl.getSessionFactory().openSession()){
+            session.beginTransaction();
+            user = session.get(User.class, id);
+            session.getTransaction().commit();
+        } catch (NoClassDefFoundError e){
+            System.out.println("Exception: " + e);
+        }
+        return user;
+    }
+}
