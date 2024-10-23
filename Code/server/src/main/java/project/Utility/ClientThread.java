8f3680008734a2d7f998bdf95f8b2896f7683e5b
KsushaTarasova <ksutaras543@mail.ru>
2024-10-23 22:59:07 +0300

    Add constructor with one parameter (Socket)

--- Code/server/src/main/java/project/Utility/ClientThread.java
+++ Code/server/src/main/java/project/Utility/ClientThread.java
@@ -1,6 +1,8 @@
 package project.Utility;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -9,6 +11,11 @@ public class ClientThread {
     private BufferedReader in;
     private PrintWriter out;
 
+    public ClientThread(Socket clientSocket) throws IOException {
+        this.clientSocket = clientSocket;
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out = new PrintWriter(clientSocket.getOutputStream());
+    }
 
 
 }
