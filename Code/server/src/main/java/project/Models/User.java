1829c6e4c4b597c63e006f196f539cec6bf58c99
KsushaTarasova <ksutaras543@mail.ru>
2024-10-24 16:24:23 +0300

    Change parameter's names

--- Code/server/src/main/java/project/Models/User.java
+++ Code/server/src/main/java/project/Models/User.java
@@ -2,14 +2,18 @@ package project.Models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import project.Converters.RolesConverter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "user")
 public class User {
@@ -21,10 +25,13 @@ public class User {
     String username;
     @Column(name="password")
     String password;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_details_id")
     UserDetails userDetails;
     @ManyToOne
     @JoinColumn(name="role_id")
     Role role;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    Set<Account> accounts = new HashSet<>();
 }
